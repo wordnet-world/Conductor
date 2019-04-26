90d878bed0e548e3726585de6c5900036fde33b1
Christopher Keinsley <keinsleychris@gmail.com>
2019-04-26 11:02:19 -0400

    Added kafka stuff

--- database/Database.go
+++ database/Database.go
@@ -1,1 +1,9 @@
 package database
+
+// Broker is an interface which allows you to Publish a message
+// and subscribe to a particular topic with an action to take
+type Broker interface {
+	Connect()
+	Publish(message string)
+	Subscribe(topic string, action func(string))
+}

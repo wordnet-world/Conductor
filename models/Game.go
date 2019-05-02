b97eff6fccb285a39b9695f4bfbc33520052d8f1
Collin Moore <collin.moore96@gmail.com>
2019-05-02 16:57:56 -0400

    Changed id's to ints

--- models/Game.go
+++ models/Game.go
@@ -3,7 +3,7 @@ package models
 // Game is a model for a single game instance
 type Game struct {
 	Name      string   `json:"name"`
-	ID        string   `json:"gameId"`
+	ID        int      `json:"gameId"`
 	StartNode string   `json:"startNode"` // probably unique id, will need a function that returns me this when querying neo4j
 	TimeLimit int      `json:"timeLimit"` // probably in seconds or minutes
 	Teams     []string `json:"teams"`
@@ -12,6 +12,6 @@ type Game struct {
 // Team is a model of a single team in a Game
 type Team struct {
 	Name  string `json:"name"`
-	ID    string `json:"teamId"`
+	ID    int    `json:"teamid"`
 	Score string `json:"score"`
 }
